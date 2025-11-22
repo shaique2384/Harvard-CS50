0187478c4921b188512f4c56d0d71838341de9ad
shaique2384 <shaique2384@gmail.com>
2025-11-22 00:16:32 -0800

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,30 @@
- # Ask user for their name
-name = input("What's your name? ")
-print("Hello,")
-print(name)
+if False:
+    # Computer understands 0s and 1s
+    # Program that lets computer understand the command in a more comprehensive way
+    # Python is a program like that, program, an interprter installed in a server or pc and 
+    # we can run that interpreter which passes through the inputs, reading 
+    # from top to bottom left to right and translates into those 0s and 1s the computer can understand 
+    print("Hello, World!")
+
+if False:
+    # Ask user for their name
+    # Variables are functions with returning values
+    # They connect the input with the output, here we are connecting the name input with the print output
+    # = is an assignment operator. Its not equality operator, it assigns the value on the right to the variable on the left.
+    name = input("What's your name? ")
+    print("Hello,")
+    print(name)
+
+if False:
+    # Concatenation: joining two strings together
+    # + operator joins two strings together, as if it is a big argument
+    name = input("What's your name? ")
+    print("Hello,"+name)
+
+if True:
+    # Print takes multiple arguments separated by commas. 2, 3, 4 arguments etc
+    name = input("What's your name? ")
+    print("Hello,",name)
+    # Comma automatically adds a space between the arguments
+    # So its better to use comma than concatenation
+
